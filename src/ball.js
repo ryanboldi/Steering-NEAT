74226473e85f1c4da8510a7ae7d206b69c1c0ebc
Ryan Boldi <ryan.boldi123@gmail.com>
2019-10-13 14:38:37 +0400

    added most of the ball class
    started adding eyes

---
+++ src/ball.js
@@ -0,0 +1,32 @@
+class Ball {
+    constructor() {
+        //this.brain = genome;
+        //this.brain.score = 0;
+        this.r = BALL_RADIUS;
+        var padding = this.r; // stops balls spawning too close to the edge
+        this.pos = createVector(random(padding, WIDTH - padding), BALL_START); //puts ball at random x
+        this.vel = createVector(0, 0);
+        this.rotation = 0; //degrees from north
+    }
+
+    show() {
+        push();
+        translate(this.pos.x, this.pos.y);
+        rotate(this.rotation);
+        fill(0);
+        noStroke();
+        ellipse(0, 0, this.r, this.r); //draws main circle
+        triangle(0, this.r / 1.9, 0, -this.r / 1.9, -this.r * 2, 0); //draws triangle to see direction
+
+        stroke(0,255,0);
+        strokeWeight(1);
+        //draw eyes (x,0), (xcos45,xsin45), (xcos45, -xsin45)
+        line(0, 0, BALL_SIGHT, 0); //middle eye
+        line(0, 0, cos(45) * BALL_SIGHT, sin(45) * BALL_SIGHT); 
+        line(0,0, cos(45) * BALL_SIGHT, -sin(45) * BALL_SIGHT);
+        
+        pop();
+    }
+    update() {}
+
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
 const WIDTH = 800;
 const HEIGHT = 600;
 
+const BALL_RADIUS = 10; //radius of ball creatures
+const BALL_START = 10; // y value that balls should start at
+const BALL_SIGHT = 50;
+
+
+
+let b;
+
 function setup() {
+    angleMode(DEGREES);
     createCanvas(WIDTH, HEIGHT);
     background(230);
+    b = new Ball();
 }
 
 function draw() {
+    background(230);
+    b.show();
+}
 
+function keyPressed() {
+    if (keyCode == ARROW_LEFT) {
+        b.rotation += 3;
+    }
+    if (keyCode == ARROW_RIGHT) {
+        b.rotation += -3;
+    }
 }
